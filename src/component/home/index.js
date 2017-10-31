6649a257307c85a3c8e180936cf4874d37753394
chenjm <chenjianming@gezhigene.com>
2017-10-31 18:07:15 +0800

    new

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
 import { Link, IndexLink } from "react-router";
+import { Icon, Popover  } from 'antd';
 import logo from "../../logo.svg";
 import "./home.scss";
 import avartar from '../../images/avatar.jpg';
+
 class Home extends Component {
+    constructor(){
+        super();
+        this.state = {
+            visible: false,
+        }
+    }
+    
+    hide = () => {
+        this.setState({
+            visible: false,
+        });
+    }
+    handleVisableChange = (visible) => {
+        this.setState({
+            visible
+        });
+    }
+    navContent = () => {
+        return (
+            <ul>
+                <li>
+                    <Link to="/music" activeClassName="navActive">
+                        <Icon type="play-circle-o" />Music
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/page3" activeClassName="navActive">
+                        Page3 
+                    </Link>
+                </li>
+            </ul>
+        )
+    }
     render() {
         return (
             <div className="App">
@@ -25,9 +60,19 @@ class Home extends Component {
                                     Page3 
                                 </Link>
                             </li>
+                            <Popover  className="mobileNavigation" 
+                                placement="bottom" 
+                                title={'AppStore'} 
+                                content={this.navContent()} 
+                                trigger="click"
+                                visible={this.state.visible}
+                                onVisibleChange={this.handleVisibleChange}
+                            >
+                                <Icon type="appstore-o" className="appStore"/>
+                            </Popover>
                             <img src={logo} className="App-logo" alt="logo" />
                         </ul>
-                    </div>
+                    </div>                    
                     <div className="userBox"><img className="avartar" src={avartar}/><span className="name">沉末</span></div>
                 </header>
                 <div className="content">
